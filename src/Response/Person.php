0650042b6438a650e608a73eaba9e3619bde148b
Prasad Chinwal <prasadchinwal5@gmail.com>
2025-01-27 12:52:28 -0600

    fix: Added fullname attribute to Person, PersonName and StudentAdvisor

--- src/Response/Person.php
+++ src/Response/Person.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace Uisits\AitsApi\Response;
 
+use Spatie\LaravelData\Attributes\Computed;
 use Spatie\LaravelData\Data;
 
 class Person extends Data
 {
+    #[Computed]
+    public ?string $fullName;
+
     public function __construct(
         public string $guid,
         public string $pidm,
         public string $uin,
         public ?string $lastName,
         public ?string $firstName,
-    ) {}
+    ) {
+        $this->fullName = $this->lastName . ', ' . $this->firstName;
+    }
 }
