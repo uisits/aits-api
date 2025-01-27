665a4d23a1741fe117020ad5fdf112f84004fb29
Prasad Chinwal <prasadchinwal5@gmail.com>
2025-01-27 10:54:20 -0600

    fix: Removed unused logging and added helpers to studentRecord and Advisor response

--- src/Response/StudentAdvisor/Advisor.php
+++ src/Response/StudentAdvisor/Advisor.php
@@ -9,10 +9,23 @@ use Uisits\AitsApi\Response\Person;
 
 class Advisor extends Data
 {
+    public ?string $firstName = null;
+
+    public ?string $lastName = null;
+
+    public ?string $uin = null;
+
+    public ?string $pidm = null;
+
     public function __construct(
         public Person $person,
         public ?AdvisorTerm $advisorTerm,
         public string $primaryAdvisorInd,
         public ?AdvisorType $advisorType,
-    ) {}
+    ) {
+        $this->firstName = $this->person?->firstName;
+        $this->lastName = $this->person?->lastName;
+        $this->uin = $this->person?->uin;
+        $this->pidm = $this->person?->pidm;
+    }
 }
